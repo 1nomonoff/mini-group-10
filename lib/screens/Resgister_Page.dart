8fc24e70a7acbebcc608c7683b04ba9a1e67188a
Пользователь <kumushabdunabiyeva143@gmail.com>
2026-03-31 13:34:41 +0300

    ....

---
+++ lib/screens/Resgister_Page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ResgisterPage extends StatelessWidget {
+const ResgisterPage({super.key});
+ @override
+  Widget build(BuildContext context) {
+return Scaffold(appBar: AppBar(),body: SingleChildScrollView(child: Column(children: [],)),);
+  }  
+}
